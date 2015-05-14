70bcc40720f8d78bd49790aa96431455b04cb545
Falco Tomasetti <falco.tomasetti@gmail.com>
2015-05-15 01:21:09 +0200

    new color

--- CirclePulseDemo WatchKit Extension/AnimationThreeController.swift
+++ CirclePulseDemo WatchKit Extension/AnimationThreeController.swift
@@ -1,20 +1,20 @@
 //
-//  AnimationTwoController.swift
+//  AnimationThreeController.swift
 //  CirclePulseDemo
 //
-//  Created by Falco Tomasetti on 14.05.15.
+//  Created by Falco Tomasetti on 15.05.15.
 //  Copyright (c) 2015 Falco Tomasetti. All rights reserved.
 //
 
 import WatchKit
 import Foundation
 
-class AnimationTwoController: WKInterfaceController {
+class AnimationThreeController: WKInterfaceController {
     
     @IBOutlet weak var img: WKInterfaceImage!
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
-        img.setImageNamed("circleani2_")
+        img.setImageNamed("circleani2green_")
         img.startAnimatingWithImagesInRange(NSRange(location: 1,length: 15), duration: 1.0, repeatCount: 50)
         // Configure interface objects here.
     }
